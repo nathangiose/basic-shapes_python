0069c9a8653cc93f2e9ce993b1020e9c660f661e
nathangiose <nathangiose@gmail.com>
2021-04-25 17:42:50 +0200

    first commit

---
+++ basic_shape.py
@@ -0,0 +1,21 @@
+# A basic square consists of
+# underscores, hyphens, and pipes
+
+
+print(" __________________ ")
+print("|                  |")
+print("|                  |")
+print("|                  |")
+print("|                  |")
+print("|                  |")
+print(" ------------------ ")
+
+#  A basic triangle consists of
+#  forward and back slashes with hyphens
+
+
+print("          ^            ")
+print("         / \           ")
+print("        /   \          ")
+print("       /     \         ")
+print("      _________        ")
